9bcafa0f7313d96ecebb7c7bf18b835b92dc9532
Javier Izquierdo Gomez <javier.izquierdo.gomez@gmail.com>
2020-02-07 18:28:59 +0100

    change package

--- spring-advanced-34-web/src/main/java/org/course/spring/controllers/PrimerControlador.java
+++ spring-advanced-34-web/src/main/java/org/course/spring/controllers/PrimerControlador.java
@@ -1,12 +1,3 @@
-/*
- * PrimerControlador.java
- *
- * Created on 10 de junio de 2007, 21:08
- *
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
- */
-
 package org.course.spring.controllers;
 
 import java.util.Date;
@@ -15,15 +6,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
-/**
- *
- * @author user
- */
 public class PrimerControlador implements Controller{
-    
-    /** Creates a new instance of PrimerControlador */
-    public PrimerControlador() {
-    }
 
     @Override
     public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
